20461e7c40db893df143783e437a2a07231fcd94
Wilson Kameni <Wilson.kameni001@umb.edu>
2021-02-02 23:09:58 -0500

    Initial commit

---
+++ src/com/wilson/BankingApplication.java
@@ -0,0 +1,118 @@
+package com.wilson;
+
+import java.util.Scanner;
+
+public class BankingApplication {
+
+    public static void main(String[] args) {
+        BankAccount obj1 = new BankAccount("Wilson", "BOA0001234");
+        obj1.showMenu();
+    }
+}
+
+//        Picking 5 options our bank accounts return
+        class BankAccount {
+            int balance;
+            int previousTransaction;
+            String customerName;
+            String customerId;
+
+            BankAccount(String cname, String cId)
+            {
+                customerName = cname;
+                customerId = cId;
+            }
+
+
+
+            void deposit(int amount) {
+                if (amount != 0) {
+                    balance = balance + amount;
+                    previousTransaction = amount;
+                }
+            }
+
+            void withdraw(int amount) {
+                if (amount != 0) {
+                    balance = balance - amount;
+                    previousTransaction = -amount;
+                }
+            }
+
+            void getPreviousTransaction() {
+                if (previousTransaction > 0) {
+                    System.out.println("Deposited: " + previousTransaction);
+                } else if (previousTransaction < 0) {
+                    System.out.println("Withdraw: " + Math.abs(previousTransaction));
+                } else {
+                    System.out.println("No transaction occurred");
+                }
+            }
+
+
+            void showMenu()
+            {
+                char option = '\0';
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.println("Welcome " + customerName);
+                System.out.println("Your ID is " + customerId);
+                System.out.println("\n");
+                System.out.println("A. Check Balance");
+                System.out.println("B. Deposit");
+                System.out.println("C. Withdraw");
+                System.out.println("D. Previous Transaction");
+                System.out.println("E. Exit");
+
+                do {
+                    System.out.println("===============================================");
+                    System.out.println("Enter an Option");
+                    System.out.println("=================================================");
+                    option = scanner.next().charAt(0);
+                    System.out.println("\n");
+
+                    switch (option) {
+                        case 'A':
+                            System.out.println("================================");
+                            System.out.println("Balance = " + balance);
+                            System.out.println("---------------------------------");
+                            System.out.println('\n');
+                            break;
+
+                        case 'B':
+                            System.out.println("===============================");
+                            System.out.println("Enter an amount to deposit:");
+                            System.out.println("---------------------------------");
+                            int amount = scanner.nextInt();
+                            deposit(amount);
+                            System.out.println('\n');
+                            break;
+
+                        case 'C':
+                            System.out.println("===============================");
+                            System.out.println("Enter an amount to withdraw:");
+                            int amount2 = scanner.nextInt();
+                            withdraw(amount2);
+                            System.out.println('\n');
+                            break;
+
+                        case 'D':
+                            System.out.println("===============================");
+                            getPreviousTransaction();
+                            System.out.println("---------------------------------");
+                            System.out.println('\n');
+                            break;
+
+                        case 'E':
+                            System.out.println("******************");
+                            break;
+
+                        default:
+                            System.out.println("Invalid option, please try again");
+                            break;
+                        }
+                }while (option != 'E') ;
+                        System.out.println("Thank You for Using our Service");
+
+                    }
+                }
---
+++ src/com/wilson/ScannerTest.java
@@ -0,0 +1,14 @@
+package com.wilson;
+
+import java.util.Scanner;
+
+public class ScannerTest {
+	public static void main(String[] args){
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("Enter a number");
+//		int x = scanner.nextInt();
+//		System.out.println("Value = " + x);
+
+	}
+}
+
